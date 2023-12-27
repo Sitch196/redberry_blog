3a932ffdf0ac9e44a6f23d6249b939b4f2bb4378
Sitch196 <sichinava1996@gmail.com>
2023-12-27 19:47:24 +0400

    fixing minor bugs

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,17 +2,16 @@
 import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import LoadingSpinner from "../../LoadingSpinner";
+import LoadingSpinner from "../../utils/LoadingSpinner";
 import exitArrow from "../../assets/exitArrow.png";
 import Image from "next/image";
 import HeaderJustLogo from "@/components/HeaderJustLogo";
 import { BlogCardProps } from "@/types";
 import { BlogDetailsProps } from "@/types";
-import { colorCombos } from "@/utils/colorCombos";
 
 const BlogPage: React.FC<BlogDetailsProps> = ({ params }) => {
   const [blogDetails, setBlogDetails] = useState<BlogCardProps | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchBlog = async () => {
@@ -32,7 +31,6 @@ const BlogPage: React.FC<BlogDetailsProps> = ({ params }) => {
         setIsLoading(false);
       } catch (err) {
         console.log(err);
-        setIsLoading(false);
       }
     };
 
@@ -40,6 +38,18 @@ const BlogPage: React.FC<BlogDetailsProps> = ({ params }) => {
       fetchBlog();
     }
   }, [params.id]);
+
+  const colorCombos = [
+    { text: "text-blue-800", bg: "bg-blue-300" },
+    { text: "text-green-800", bg: "bg-green-300" },
+    { text: "text-red-800", bg: "bg-red-300" },
+    { text: "text-yellow-800", bg: "bg-yellow-300" },
+    { text: "text-purple-800", bg: "bg-purple-300" },
+    { text: "text-indigo-800", bg: "bg-indigo-300" },
+    { text: "text-pink-800", bg: "bg-pink-300" },
+    { text: "text-orange-800", bg: "bg-orange-300" },
+    { text: "text-teal-800", bg: "bg-teal-300" },
+  ];
 
   return (
     <motion.div
@@ -49,31 +59,31 @@ const BlogPage: React.FC<BlogDetailsProps> = ({ params }) => {
       transition={{ duration: 0.3 }}
       className="flex flex-col bg-white "
     >
-      <div className="flex justify-center items-center border-b-2 ">
+      <div className="flex justify-center items-center border-b-2 sticky top-0 bg-white ">
         <HeaderJustLogo />
       </div>
 
-      {isLoading || blogDetails === null ? (
-        <LoadingSpinner />
-      ) : (
-        <motion.div
-          key={blogDetails?.id}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 20 }}
-          transition={{ duration: 0.3 }}
-          className="w-[720px] mx-auto mt-8"
-        >
-          <Link href="/">
-            <Image
-              src={exitArrow}
-              width={44}
-              height={44}
-              alt="exit"
-              className="absolute top-[200px] left-[250px]  "
-            />
-          </Link>
-          <div className="p-4">
+      <motion.div
+        key={blogDetails?.id}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 20 }}
+        transition={{ duration: 0.3 }}
+        className="w-[720px] mx-auto mt-8"
+      >
+        <Link href="/">
+          <Image
+            src={exitArrow}
+            width={44}
+            height={44}
+            alt="exit"
+            className="absolute top-[200px] left-[250px]  "
+          />
+        </Link>
+        <div className="p-4">
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : (
             <motion.img
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
@@ -83,63 +93,63 @@ const BlogPage: React.FC<BlogDetailsProps> = ({ params }) => {
               src={blogDetails?.image}
               alt={blogDetails?.title}
             />
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 20 }}
-              transition={{ duration: 0.3 }}
-              className="text-gray-700 text-base mt-[40px] text-[16px] font-bold"
-            >
-              {blogDetails?.author}
-            </motion.p>
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 20 }}
-              transition={{ duration: 0.3 }}
-              className="text-gray-600  mt-[10px] text-xs"
-            >
-              {blogDetails?.publish_date} &#8226; {blogDetails?.email}
-            </motion.p>
-            <motion.h1
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 20 }}
-              transition={{ duration: 0.3 }}
-              className="text-3xl font-bold mt-[24px] text-[32px]"
-            >
-              {blogDetails?.title}
-            </motion.h1>
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 20 }}
-              transition={{ duration: 0.3 }}
-              className="text-gray-700 text-sm mt-[24px]"
-            >
-              {blogDetails?.categories.map((category, index) => (
-                <span
-                  key={index}
-                  className={`${colorCombos[index % colorCombos.length].text} ${
-                    colorCombos[index % colorCombos.length].bg
-                  } inline-block rounded-full px-3 py-1 mr-2`}
-                >
-                  {category.title}
-                </span>
-              ))}
-            </motion.p>
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 20 }}
-              transition={{ duration: 0.3 }}
-              className="text-gray-600 font-medium text-[16px] mt-[40px]"
-            >
-              {blogDetails?.description}
-            </motion.p>
-          </div>
-        </motion.div>
-      )}
+          )}
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
+            className="text-gray-700 text-base mt-[40px] text-[16px] font-bold"
+          >
+            {blogDetails?.author}
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
+            className="text-gray-600  mt-[10px] text-xs"
+          >
+            {blogDetails?.publish_date} &#8226; {blogDetails?.email}
+          </motion.p>
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
+            className="text-3xl font-bold mt-[24px] text-[32px]"
+          >
+            {blogDetails?.title}
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
+            className="text-gray-700 text-sm mt-[24px]"
+          >
+            {blogDetails?.categories.map((category, index) => (
+              <span
+                key={index}
+                className={`${colorCombos[index % colorCombos.length].text} ${
+                  colorCombos[index % colorCombos.length].bg
+                } inline-block rounded-full px-3 py-1 mr-2`}
+              >
+                {category.title}
+              </span>
+            ))}
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
+            className="text-gray-600 font-medium text-[16px] mt-[40px]"
+          >
+            {blogDetails?.description}
+          </motion.p>
+        </div>
+      </motion.div>
     </motion.div>
   );
 };
